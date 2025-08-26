6e5c6633e5ed4d0abcf3902d33a62f2cec54288a
pedropenhavn <pedro.afonso@vilanova.com.br>
2025-08-26 14:35:55 -0300

    Ajustes docker

--- infra/docker/production.entrypoint.sh
+++ infra/docker/production.entrypoint.sh
@@ -6,14 +6,17 @@ set -e
 # Instala as dependências do PHP com Composer
 composer install --no-interaction --no-progress --optimize-autoloader
 
-# Limpa o cache do Laravel
-php artisan optimize:clear
-
 # Verificar se a chave de criptografia do Laravel foi gerada
 if ! grep -q "^APP_KEY=base64" .env; then
     # Gera a chave de criptografia do Laravel
     php artisan key:generate
 fi
+
+# Executar migrations se necessário
+php artisan migrate --force
+
+# Limpa o cache do Laravel
+php artisan optimize:clear
 
 # Iniciar o supervisor para gerenciar os processos
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
